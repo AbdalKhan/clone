981ac265fce5276750d9b9b51a02dc95b61991a8
Abdal Khan <abdal_ijaz_k@hotmail.com>
2019-12-23 18:51:33 -0700

    sign in, sign out and browse

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.prepend('provide',
+environment.plugins.prepend('Provide',
 	new webpack.ProvidePlugin({
 		$: 'jquery/src/jquery',
 		jQuery: 'jquery/src/jquery',
-		Popper: ['popper.js, 'default'])
+		Popper: ['popper.js, 'default']
 	})
 )
 
